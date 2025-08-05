ac0a5e685eb56256dbfd2030430dd858cdcec81b
David Harris <dddharris2@hotmail.co.uk>
2025-08-05 17:39:37 +0100

    Removed EUR dataframe and excel writer

--- actuals_czk/main.py
+++ actuals_czk/main.py
@@ -62,97 +62,68 @@ class ActualsCzk(Frame):
         return file_path
         
     def _create_cost_dataframes(self, file_path):
-        # Creates two dataframs one for EUR and one for CZK based on the cost excel file uploaded by the user
+        # Creates one dataframe for CZK based on the cost excel file uploaded by the user
         
         columns_to_keep = ["ACCT # (konv.)", "Unnamed: 15", "DESCRIPTION", "Date", "POHODA #. (č. dokladu)"]
     
-        # Read the Excel file and create dataframes for EUR and CZK sheets
-        temp_df_eur = pd.read_excel(file_path, sheet_name="ACTUALS EUR", header=8)
+        # Read the Excel file and create dataframes for the CZK sheet
         temp_df_czk = pd.read_excel(file_path, sheet_name="ACTUALS CZK", header=8)
             
         # Replace 0.0, "", and " " with NaN 
         temp_df_czk = temp_df_czk.replace({0.0: np.nan, "": np.nan, " ": np.nan}, regex=False)
-        temp_df_eur = temp_df_eur.replace({0.0: np.nan, "": np.nan, " ": np.nan}, regex=False)
        
         # Keep only the specified columns and drop completely empty columns
-        df_eur_cost = temp_df_eur[[col for col in columns_to_keep if col in temp_df_eur.columns]]
         df_czk_cost = temp_df_czk[[col for col in columns_to_keep if col in temp_df_czk.columns]]
         
         # Drop rows where all cells in the row are NaN. Retain rows with actual data
-        df_eur_cost = df_eur_cost.dropna(how='all')
         df_czk_cost = df_czk_cost.dropna(how='all')
         
         # Fill NaN values in rows with data that used to be 0.0, "", or " "
-        df_eur_cost = df_eur_cost.fillna(0.00)
         df_czk_cost = df_czk_cost.fillna(0.00)
         
-        return df_eur_cost, df_czk_cost
+        return df_czk_cost
     
-    def _transfer_dataframes(self, df_czk_cost, df_eur_cost):
+    def _transfer_dataframes(self, df_czk_cost):
         # Transfers data from the cost dataframes to the import dataframes
-        df_eur_import, df_czk_import = self._create_import_dataframes()
+        df_czk_import = self._create_import_dataframes()
 
         # Iterate through the columns and fill the import dataframes with the appropriate values
-        for col in df_eur_import.columns:
+        for col in df_czk_import.columns:
             if col == "Kostenart":
                 # Remove the last 4 characters from the "ACCT # (konv.)" column and append "0000"
-                df_eur_import[col] = df_eur_cost["ACCT # (konv.)"].astype(str).str[:-4] + "0000"
                 df_czk_import[col] = df_czk_cost["ACCT # (konv.)"].astype(str).str[:-4] + "0000"
             elif col == "Kostenstelle":
                 # Static value for "Kostenstelle"
-                df_eur_import[col] = "NF"
                 df_czk_import[col] = "NF"
             elif col == "Kostenträger":
                 # Static value for "Kostenträger"
-                df_eur_import[col] = "1015-70108-01-1"
                 df_czk_import[col] = "1015-70108-01-1"
             elif col == "Betrag":
                 # Format the "Unnamed: 15" column (column p) as a float with two decimal places 
-                df_eur_import[col] = df_eur_cost["Unnamed: 15"].apply(lambda x: f"{x:.2f}")
                 df_czk_import[col] = df_czk_cost["Unnamed: 15"].apply(lambda x: f"{x:.2f}")
             elif col == "Belegdatum":
                 # Use the "Date" column for "Belegdatum"
-                df_eur_import[col] = pd.to_datetime(df_eur_cost["Date"]).dt.strftime("%d.%m.%Y")
                 df_czk_import[col] = pd.to_datetime(df_czk_cost["Date"]).dt.strftime("%d.%m.%Y")
             elif col == "Belegnummer":
                 # Use the "POHODA #. (č. dokladu)" column for "Belegnummer"
-                df_eur_import[col] = df_eur_cost["POHODA #. (č. dokladu)"]
                 df_czk_import[col] = df_czk_cost["POHODA #. (č. dokladu)"]
             elif col == "Belegtext":
                 # Use the "DESCRIPTION" column for "Belegtext"
-                df_eur_import[col] = df_eur_cost["DESCRIPTION"]
                 df_czk_import[col] = df_czk_cost["DESCRIPTION"]
                 
-        return df_czk_import, df_eur_import
+        return df_czk_import
 
-    def save_file(self, df_eur_import, df_czk_import):
+    def save_file(self, df_czk_import):
         
         # Format 'Betrag' column with comma as decimal separator if it exists
-        for df in [df_czk_import, df_eur_import]:
-            df["Betrag"] = df["Betrag"].astype(str).str.replace(".", ",", regex=False)
+        df_czk_import["Betrag"].astype(str).str.replace(".", ",", regex=False)
         
-        # txt file are uploaded into the system 
-        df_czk_import.to_csv(os.path.join(self.menu_bar.output_dir_path, "ACTUALS CZK.txt"), sep="\t", index=False)
-        df_eur_import.to_csv(os.path.join(self.menu_bar.output_dir_path, "ACTUALS EUR.txt"), sep="\t", index=False)
-        
-        # Saves the import dataframes to an Excel file with auto-adjusted column widths
-        # Not upload to the system. Can be used for reference and verification
-        file_path = os.path.join(self.menu_bar.output_dir_path, "Actual Cost_Sesam_Import.xlsx")
+        # txt file are uploaded into the system
+        file_path = os.path.join(self.menu_bar.output_dir_path, "ACTUALS CZK.txt")
         try:
-            with pd.ExcelWriter(file_path) as writer:
-                df_czk_import.to_excel(writer, index=False, sheet_name="ACTUALS CZK")
-                df_eur_import.to_excel(writer, index=False, sheet_name="ACTUALS EUR")
-                
-                
-                worksheets = [writer.sheets["ACTUALS CZK"], writer.sheets["ACTUALS EUR"]]
-
-                for worksheet in worksheets:
-                    for col in worksheet.columns:
-                        max_length = max(len(str(cell.value)) for cell in col)
-                        worksheet.column_dimensions[get_column_letter(col[0].column)].width = max_length
-                        
-            # Show a message box to indicate the file has been saved successfully or not.
-            if os.path.exists(file_path):
-                messagebox.showinfo("Success", "Data Copy Successful. \nFile saved as 'Actual Cost_Sesam_Import.xlsx'")
+            df_czk_import.to_csv(file_path, sep="\t", index=False, encoding="utf-8-sig")
         except Exception as e:
             messagebox.showerror("Save Failed", f"An error occurred while saving the file:\n{str(e)}")
+        finally:
+            if os.path.exists(file_path):
+                messagebox.showinfo("Success", "Data Copy Successful. \nFile saved as 'Actual Cost_Sesam_Import.xlsx'")
