4730cc2e6ebb24c6f431572cf15d8b6f08ec5ea7
David Harris <dh1a25@soton.ac.uk>
2025-09-23 15:33:19 +0100

    periode from Date

--- actuals_it/main.py
+++ actuals_it/main.py
@@ -99,6 +99,7 @@ class ActualsIT(Frame):
             # If there's a valid "Data Doc." and current_code is set, create a new row as all information needed is on that row
             if pd.notna(row["Data Doc."]) and row["Data Doc."] != 0.0 and current_code:
                 date = pd.to_datetime(row["Data Doc."])
+                date = pd.to_datetime(row["Data Doc."])
                 periode = 12 if date.month == 1 else date.month - 1
                 betrag = f"{row['Unnamed: 17']:.2f}".replace(".", ",")
                 new_row = {
